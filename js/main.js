332bc62b07e3fcbab730ee623e0115855797e0b9
FrancescoGiommi <francescogiommi97@icloud.com>
2024-11-07 17:29:56 +0100

    Add google font and title size

--- js/main.js
+++ js/main.js
@@ -16,16 +16,19 @@ Bonus
 rendi la pagina responsive, in modo che su mobile e tablet le foto si dispongano man mano una sotto l’altra 
 ed il titolo abbia una dimensione adeguata */
 
+/* dichiaro l'ID della card da generare */
 const postsCardEl = document.getElementById("posts-card");
 let cardPost;
 
+/* genero l'immagine richiamndo l'API con fetch */
 fetch("https://jsonplaceholder.typicode.com/photos?_limit=6")
   .then((response) => response.json())
   .then((posts) => {
     console.log(posts);
+    /* genero le card con il metodo forEach */
     posts.forEach((post) => {
       postsCardEl.innerHTML += `
-        <div class="col-4 ">
+        <div class="col-4">
             <div class="card">
               <img class="m-3" src="${post.url}" alt="" />
               <div class="card-body">
